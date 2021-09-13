a8c5b6ae5c5c84bdbb3516695059c97157b2c814
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-09-13 19:30:27 -0300

    solucionando problemas de dinosaurio rigido y preparando para tutorial de C++

--- Codigos/dinosaruio.gd
+++ Codigos/dinosaruio.gd
@@ -11,12 +11,12 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func _physics_process(delta):#esta funcion se repite 60 veces por segundos y esta optimizada para físicas
-	movimiento.y -= 1#esto representa la gravedad en un cuerpo kinematico
-	movimiento.z = velocidad * delta #esto mueve en cada segundo el personaje en el eje z
+# func _physics_process(delta):#esta funcion se repite 60 veces por segundos y esta optimizada para físicas
+# 	movimiento.y -= 1#esto representa la gravedad en un cuerpo kinematico
+# 	movimiento.z = velocidad * delta #esto mueve en cada segundo el personaje en el eje z
 	
-	move_and_slide(movimiento)
-	print(get_slide_count())
+# 	move_and_slide(movimiento)
+# 	print(get_slide_count())
 
 
 	
--- Codigos/dinosaruio_rigid.gd
+++ Codigos/dinosaruio_rigid.gd
@@ -1,31 +1,20 @@
 extends RigidBody
 
-export var impulso = 10#variable que cambio desde el editor para determinar el impulso
-var impulso_Vector = Vector3(0,impulso,0)#esto va a ser el impulso
+export var impulso : float = 12#variable que cambio desde el editor para determinar el impulso
 var posicion = Vector3()#esto determina la posicion donde va a estar
 var puede_saltar = false
-var velocidad #determina la velocidad del dinosaurio
+export var velocidad : float = 5#determina la velocidad del dinosaurio
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocidad = linear_velocity.z #en un comienzo la velocidad va a ser 5
+	#velocidad = 5 #en un comienzo la velocidad va a ser 5
+	pass
 
-func _physics_process(delta):
-	if linear_velocity.z < velocidad:#si la velocidad lineal es menor a la velocidad
-		linear_velocity.z = velocidad#la velocidad lineal va a ser igual a la velocidad
+# func _physics_process(delta):
+# 	# linear_velocity.z = velocidad#la velocidad lineal va a ser igual a la velocidad
+# 	pass
 	
-	#var esta_en_colision = $Area.overlaps_area(get_tree().get_nodes_in_group("suelo_area")[0])
-	posicion = translation#la posicion es la posición donde esta el dinosaurio
-	if (Input.is_action_just_pressed("click_izquierdo") or Input.is_action_just_pressed("click_touch")) and puede_saltar:#si presiono click izquierdo
-		get_tree().get_nodes_in_group("saltar")[0].playing = true #activa sonido saltar
-		get_tree().get_nodes_in_group("dinosaurio_particulas")[0].emitting = false#deja de emitir particulas en el peso
-		get_tree().get_nodes_in_group("dinosaurio_particulas")[0].lifetime = 0.3#el nodo de las particulas no es visible
-		puede_saltar = false#no puede saltar hasta que esto sea verdero
-		apply_impulse(posicion,impulso_Vector)#impulso hace que el personaje salte
-		$AnimationTree["parameters/combinar/blend_amount"] = 0.8#cambia la animacion a saltar
-	
-
-func _on_Area_area_entered(area):
+func _on_Area_area_entered(area : Area):
 	if area.is_in_group("obstaculo"):#si el area esta en el grupo obstaculo
 		get_tree().get_nodes_in_group("sonido_fondo")[0].playing = false#activa sonido morir
 		get_tree().get_nodes_in_group("morir")[0].playing = true#activa sonido morir
@@ -34,12 +23,29 @@ func _on_Area_area_entered(area):
 	
 	#esto es con el area
 	if area.is_in_group("suelo_area"):#si colisiona con el suelo
-		if puede_saltar == false:
-			get_tree().get_nodes_in_group("caer")[0].playing = true#activa sonido morir
-			puede_saltar = true#puede saltar
-			$AnimationTree["parameters/combinar/blend_amount"] = 0#cambia la animacion a correr
-			get_tree().get_nodes_in_group("dinosaurio_particulas")[0].emitting = true#emite particulas en el piso
-			get_tree().get_nodes_in_group("dinosaurio_particulas")[0].lifetime = 0.5#el nodo de las particulas es visible
+		# if puede_saltar == false:
+		linear_velocity.z = velocidad
+		puede_saltar = true#puede saltar
+		print(puede_saltar)
+		get_tree().get_nodes_in_group("caer")[0].playing = true#activa sonido morir
+		$AnimationTree["parameters/combinar/blend_amount"] = 0#cambia la animacion a correr
+		get_tree().get_nodes_in_group("dinosaurio_particulas")[0].emitting = true#emite particulas en el piso
+		get_tree().get_nodes_in_group("dinosaurio_particulas")[0].lifetime = 0.5#el nodo de las particulas es visible
 
+func _input(event):
+	if (Input.is_action_just_pressed("click_izquierdo") or Input.is_action_just_pressed("click_touch")):#si presiono click izquierdo
+		if puede_saltar == true:
+			puede_saltar = false#no puede saltar hasta que este en el suelo
+			print(puede_saltar)
+			posicion = translation
+			#linear_velocity.y = 0
+			apply_impulse(posicion,Vector3(0,impulso,1))#impulso hace que el personaje salte
+			get_tree().get_nodes_in_group("saltar")[0].playing = true #activa sonido saltar
+			get_tree().get_nodes_in_group("dinosaurio_particulas")[0].emitting = false#deja de emitir particulas en el peso
+			get_tree().get_nodes_in_group("dinosaurio_particulas")[0].lifetime = 0.3#el nodo de las particulas no es visible
+			$AnimationTree["parameters/combinar/blend_amount"] = 0.8#cambia la animacion a saltar
+
+			
 func _on_Timer_timeout():#si el tiempo termina
 	velocidad += 1#aumento en 1 la velocidad
+	
